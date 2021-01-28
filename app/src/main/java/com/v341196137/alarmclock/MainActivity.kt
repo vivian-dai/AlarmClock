053162dddcfb5e7c64b0069a1a0374a2e829adc7
v341196137 <38384400+v341196137@users.noreply.github.com>
2021-01-28 18:12:19 -0500

    sketchy code that does absolutely nothing :D

--- app/src/main/java/com/v341196137/alarmclock/MainActivity.kt
+++ app/src/main/java/com/v341196137/alarmclock/MainActivity.kt
@@ -2,6 +2,8 @@ package com.v341196137.alarmclock
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+
 // do most of our work in here
 // main hub for changing layouts and views and stuff
 // buttononclick events to change layouts when clicked on
@@ -10,5 +12,30 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         print(R.id.time_text_view)
+
+        //bad coding coventions B) (probably should use binding later)
+        val alarmButton: Button = findViewById(R.id.alarm_button)
+        val timerButton: Button = findViewById(R.id.timer_button)
+        val stopwatchButton: Button = findViewById(R.id.stopwatch_button)
+
+        //button listeners
+        alarmButton.setOnClickListener(){ switchToAlarm()}
+        timerButton.setOnClickListener(){switchToTimer()}
+        stopwatchButton.setOnClickListener(){ switchToStopwatch()}
+
     }
+}
+
+
+//TODO: hahahaha implement all this
+fun switchToAlarm(){
+
+}
+
+fun switchToTimer(){
+
+}
+
+fun switchToStopwatch(){
+
 }
