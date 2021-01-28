2035d2df404b6dc44c79ca87f12e5801bca48c44
Sherwin Chiu <sherwin.chiu89@gmail.com>
2021-01-28 17:31:07 -0500

    yay commits are fun

--- app/src/main/java/com/v341196137/alarmclock/AlarmAdapter.kt
+++ app/src/main/java/com/v341196137/alarmclock/AlarmAdapter.kt
@@ -2,11 +2,12 @@ package com.v341196137.alarmclock
 
 import android.view.View
 import androidx.recyclerview.widget.RecyclerView
-
+/*
 class AlarmAdapter : RecyclerView.Adapter() {
 
     class ViewHolder(alarm_item: View) : RecyclerView.ViewHolder(alarm_item){ // will contain instance of AlarmData --> alarm_item.xml
-        val
+
 
     }
 }
+*/
--- app/src/main/java/com/v341196137/alarmclock/MainActivity.kt
+++ app/src/main/java/com/v341196137/alarmclock/MainActivity.kt
@@ -2,11 +2,13 @@ package com.v341196137.alarmclock
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-
+// do most of our work in here
+// main hub for changing layouts and views and stuff
+// buttononclick events to change layouts when clicked on
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+        print(R.id.time_text_view)
     }
 }
