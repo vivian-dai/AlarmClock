972875bfbf7a05859a7963a94f4521e8d2a36e1b
v341196137 <38384400+v341196137@users.noreply.github.com>
2021-01-31 16:09:33 -0500

    buttons are functional

--- app/src/main/java/com/v341196137/alarmclock/MainActivity.kt
+++ app/src/main/java/com/v341196137/alarmclock/MainActivity.kt
@@ -4,10 +4,8 @@ package com.v341196137.alarmclock
 // Android libraries
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
 import android.widget.Button
-import androidx.annotation.IdRes
-import kotlinx.android.synthetic.main.activity_main.*
+import android.widget.Toast
 import java.util.LinkedList
 
 // do most of our work in here
@@ -19,23 +17,14 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         var alarmList = LinkedList<AlarmData>()
 
-        //bad coding coventions B) (probably should use binding later)
-        // we can get rid all of this because just calling alarm_button == findViewById(R.Id.alarm_button) As Button
-        val alarmButton: Button = findViewById(R.id.alarm_button)
-        val timerButton: Button = findViewById(R.id.timer_button)
-        val stopwatchButton: Button = findViewById(R.id.stopwatch_button)
-        val addButton: Button = findViewById(R.id.add_alarm_button) // add button
+        initiateAlarmViewButtons()
 
-
-        //button listeners
-        // TODO: figure out if to use .setContentView or .addView
-        // Button listeners on acitivty_main.xml
-        alarmButton.setOnClickListener() {switchToView(R.layout.activity_main) } //currently operating under the assumption main view is alarm view
-        timerButton.setOnClickListener() { switchToView(R.layout.timer_view)}
-        stopwatchButton.setOnClickListener() { switchToView(R.layout.stopwatch_view) }
-        add_alarm_button.setOnClickListener(){ switchToView(R.layout.add_alarm_view) }
-        // Button listeners on add_alarm_view.xml
     }
+
+    /**
+     * Hi Sherwin you should javadoc this :P
+     * @return something something
+     */
     private fun generateList() : LinkedList<AlarmData> {
         var alarmList = LinkedList<AlarmData>()
         alarmList.add(AlarmData("12:00")) // change "12:00" to string where time is specified
@@ -49,6 +38,67 @@ class MainActivity : AppCompatActivity() {
      */
     private fun switchToView(view: Int){
         setContentView(view)
+    }
+
+    /**
+     * Initiates the three buttons at the bottom necessary for navigating
+     */
+    private fun initiateNavButtons(){
+        val alarmButton: Button = findViewById(R.id.alarm_button)
+        val timerButton: Button = findViewById(R.id.timer_button)
+        val stopwatchButton: Button = findViewById(R.id.stopwatch_button)
+        //button listeners
+        // TODO: figure out if to use .setContentView or .addView
+        // Button listeners on acitivty_main.xml
+        alarmButton.setOnClickListener() {
+            Toast.makeText(this, "alarm button pressed", Toast.LENGTH_SHORT).show()
+            switchToView(R.layout.activity_main)
+            initiateAlarmViewButtons()
+        } //currently operating under the assumption main view is alarm view
+        timerButton.setOnClickListener() {
+            Toast.makeText(this, "timer button pressed", Toast.LENGTH_SHORT).show()
+            switchToView(R.layout.timer_view)
+            initiateTimerViewButtons()
+        }
+        stopwatchButton.setOnClickListener() {
+            Toast.makeText(this, "stopwatch button pressed", Toast.LENGTH_SHORT).show()
+            switchToView(R.layout.stopwatch_view)
+            initiateStopwatchButtons()
+        }
+    }
+
+    /**
+     * Does all the button initializations necessary when switching to alarm view
+     */
+    private fun initiateAlarmViewButtons(){
+        initiateNavButtons()
+        val addAlarmButton: Button = findViewById(R.id.add_alarm_button)
+        addAlarmButton.setOnClickListener(){
+            Toast.makeText(this, "add alarm button pressed", Toast.LENGTH_SHORT).show()
+            switchToView(R.layout.add_alarm_view)
+            initiateAddAlarmButtons()
+        }
+    }
+
+    /**
+     * Does all button initialization necessary when switching to timer view
+     */
+    private fun initiateTimerViewButtons(){
+        initiateNavButtons()
+    }
+
+    /**
+     * Does all necessary button initialization for when switching to stopwatch view
+     */
+    private fun initiateStopwatchButtons(){
+        initiateNavButtons()
+    }
+
+    /**
+     * Initiates the buttons for add alarm
+     */
+    private fun initiateAddAlarmButtons(){
+
     }
 
 }
